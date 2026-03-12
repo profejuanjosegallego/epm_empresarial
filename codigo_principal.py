08f84d095824ebf93334f92332d7d5d2d32cb72c
Juan Jose Gallego <jjosegallegomesa@gmail.com>
2026-03-11 20:18:29 -0500

    feat se crea base del proyecto para utilizar listas y diccionarios

---
+++ codigo_principal.py
@@ -0,0 +1,16 @@
+#Algoritmo 1: Crear una rutina que permita crear N(muchos) usuarios para la plataforma
+idUsuario=None
+nombresUsuario=None
+documentoUsuario=None 
+epsUsuario=None
+telefonoUsuario=None
+correoUsuario=None
+contraseñaUsuario=None
+
+#Algoritmo 2: Crear una rutina que permita evaluar las credenciales de un usuario al moemnto de intentar ingresar a plataforma, si el usuario se equivoca 3 veces bloquear, si es exitoso permitir la gestión de la represa
+
+#Algoritmo 3: Crear una rutina que "SIMULE" las mediciones de la represa (Numeros enteros de 0 a 800) (200)
+
+#Algoritmo 4: Crear una rutina que permita obtener el promedio numerico de una serie de numero
+
+#Algoritmo 5: Crear una rutina que permita clasificar el promedio de las mediciones y de esa manera tomar una 3 opciones posibles (apagar alguna maquinaria, operar con normalidad, alertar y abrir compuertas)
